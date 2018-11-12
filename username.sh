5b1a4f0c8c3904f196653c5d424f1629b90a98ec
Trevor Kling <kling109@mail.chapman.edu>
2018-11-12 02:13:53 +0000

    Fixed issues

--- username.sh
+++ username.sh
@@ -1,22 +1,13 @@
 #! /bin/bash
 # username.sh
 # Trevor Kling
-echo "Enter a username: "
+echo "Enter a username.  Usernames must contain only lowercase letters, numbers, and the underscore character.
+	  Every username must start with a lowercase letter, and be between 3 and 12 characters long."
 read USER
 while echo $USER | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	if [ echo $USER | egrep -v "^[a-z]" > /dev/null 2>&1 ]
-	then
-		echo "Usernames must begin with a lowercase letter."
-	elif [ echo $USER | egrep -v "^[a-z0-9_]{1,12}" > /dev/null 2&>1 ]
-	then
-		echo "Usernames must be shorter than 13 characters."
-	elif [ echo $USER | egrep -v "^[a-z0-9_]{3,}" > /dev/null 2&>1 ]
-	then
-		echo "Usernames must be at least 3 characters."
-	else
-		echo "Usernames may only contain lowercase letters, numbers, and underscores"
-	fi
+	echo "That is not a valid username.  Usernames must contain only lowercase letters, numbers, and the underscore character.
+	     Every username must start with a lowercase letter, and be between 3 and 12 characters long."
 	read USER
 done
 echo "Thank you"
